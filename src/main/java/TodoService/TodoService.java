3d73c9c2413d37873c73e5d5efea9cef666ef3cb
arsham1010 <arsham.fotouhi.2010@gmail.com>
2025-10-02 04:54:34 +0330

    Creat Repository class

---
+++ src/main/java/TodoService/TodoService.java
@@ -0,0 +1,17 @@
+package TodoService;
+
+import Model.Todo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface TodoService {
+    void save(Todo todo);
+
+    void changeComplete(Todo todo, boolean complete);
+
+    Todo findById(Integer id);
+
+   Page<Todo> findAll(Pageable pageable);
+
+    void deleteTodoById(Integer id);
+}
